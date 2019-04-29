b09051a00aea6b9ec7cf48e84f327fbac12590d3
Andrew Grimberg <agrimberg@linuxfoundation.org>
2019-04-29 13:25:25 -0700

    Add NEXUS_URL env var
    Change-Id: I2072f1eed869c6465b8a89d4977b1287633fbb70
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- jenkins-config/global-vars-sandbox.sh
+++ jenkins-config/global-vars-sandbox.sh
@@ -2,4 +2,5 @@ GERRIT_URL=https://gerrit.oran-osc.org/r
 GIT_URL=ssh://oran-jobbuilder@gerrit.oran-osc.org:29418
 JENKINS_HOSTNAME=vex-sjc-oran-jenkins-sandbox-1
 LOGS_SERVER=https://nexus.o-ran-sc.org
+NEXUS_URL=https://nexus.o-ran-sc.org
 SILO=sandbox
