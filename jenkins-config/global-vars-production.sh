0655b12b19d235d3ea9340415b63a017f4c214e4
Andrew Grimberg <agrimberg@linuxfoundation.org>
2019-05-03 10:37:09 -0700

    Add DOCKER_REGSITRY related env vars
    Change-Id: I0bdebd06c91b1cb0728b48faeaf3081e4b09337c
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- jenkins-config/global-vars-production.sh
+++ jenkins-config/global-vars-production.sh
@@ -1,6 +1,8 @@
+DOCKER_REGISTRY=nexus3.o-ran-sc.org
 GERRIT_URL=https://gerrit.oran-osc.org/r
 GIT_URL=ssh://oran-jobbuilder@gerrit.oran-osc.org:29418
 JENKINS_HOSTNAME=vex-sjc-oran-jenkins-prod-1
 LOGS_SERVER=https://logs.o-ran-sc.org
 NEXUS_URL=https://nexus.o-ran-sc.org
+REGISTRY_PORTS=10001 10002 10003 10004
 SILO=production
