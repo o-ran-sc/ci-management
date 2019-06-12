db2347d5cb9f13ede8223c9001149e0338710f43
Lott, Christopher (cl778h) <cl778h@att.com>
2019-06-12 09:53:40 -0400

    Add shell script to extract RMr packages from builder
    Adds a post-docker script to copy deb package out of CI container.
    Also adds Jenkins merge job, because someday the merge job will
    push the deb to cloudpackage.io.
    
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>
    Change-Id: Ia3f3352fb1f87b459fe125c1b12f34c09a4a56b4

---
+++ jjb/ric-plt-lib-rmr/shell/copy-rmr-packages.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#==================================================================================
+#       Copyright (c) 2019 Nokia
+#       Copyright (c) 2018-2019 AT&T Intellectual Property.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#==================================================================================
+
+set -eux -o pipefail
+echo "--> copy-rmr-packages.sh"
+
+# extracts to host the artifacts created by the builder
+
+# This file is created by RMr library build process
+# with path(s) to the generated artifact(s)
+file="/tmp/rmr_deb_path"
+
+# Create a container from the build image by running a trivial command.
+# DOCKER_NAME and DOCKER_IMAGE_TAG are injected by previous steps
+container=$(docker run -d "$DOCKER_NAME":"$DOCKER_IMAGE_TAG" ls "$file")
+docker logs "$container"
+docker cp "$container:$file" .
+filebase=$(basename "$file")
+deb=$(cat "$filebase")
+docker cp "$container:$deb" .
