7e495ca5eb1f1176f3104161565aeb48920805c6
Lott, Christopher (cl778h) <cl778h@att.com>
2019-07-31 15:32:03 -0400

    Define Docker-CI merge template to build packages
    Revise existing library jobs to use the new merge template.
    
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>
    Change-Id: I8365ce8b37427692f8c6c436e87bd767bfa996c7

---
+++ jjb/oran-jjb/shell/packagecloud-push.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -l
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2019 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> packagecloud-push.sh"
+set -eu -o pipefail
+
+if [ ! -f ~/.packagecloud ]; then
+    echo "INFO: .packagecloud file not found"
+    exit 0
+fi
+
+ # For DEB
+count=($DEBIAN_DISTRIBUTION_VERSION)
+echo ${count[@]}
+debs=$(find . -type f -iname '*.deb')
+# modern bash syntax is helpful
+for (( i = 0; i < ${#count[@]}; i++ )); do
+    for deb in $debs; do
+       echo "Pushing $deb $PACKAGECLOUD_ACCOUNT/$PACKAGECLOUD_REPO/${count[i]}"
+       package_cloud push $PACKAGECLOUD_ACCOUNT/$PACKAGECLOUD_REPO/${count[i]} $deb
+    done
+done
+
+# For RPM
+count=($RPM_DISTRIBUTION_VERSION)
+echo ${count[@]}
+rpms=$(find . -type f -iregex '.*/.*\.\(s\)?rpm')
+# modern bash syntax is helpful
+for (( i = 0; i < ${#count[@]}; i++ )); do
+    for rpm in $rpms; do
+       echo "Pushing $rpm $PACKAGECLOUD_ACCOUNT/$PACKAGECLOUD_REPO/${count[i]}"
+       package_cloud push $PACKAGECLOUD_ACCOUNT/$PACKAGECLOUD_REPO/${count[i]} $rpm
+    done
+done
