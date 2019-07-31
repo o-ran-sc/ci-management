2cd087b58f0f5524b794eda8f4d5290cacb22ed1
Lott, Christopher (cl778h) <cl778h@att.com>
2019-07-31 08:48:26 -0400

    Refactor package-export script for reuse
    Simplifies and renames from copy-rmr-packages to export-packages
    so the script can be reused by many projects, starting with mdclog.
    
    Change-Id: If25ad3ac6f4bc7df99707cebba335e001b0b01fe
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>

---
+++ jjb/shell/export-packages.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#==================================================================================
+#       Copyright (c) 2019 AT&T Intellectual Property.
+#       Copyright (c) 2019 Nokia
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#==================================================================================
+
+set -eux -o pipefail
+echo "--> export-packages.sh"
+
+# Launches the docker image, which has an entrypoint script
+# that copies artifacts created by the builder to a directory.
+# Environment variables are injected in previous Jenkins steps.
+HOST=/tmp/export
+GUEST=/export
+mkdir -p "$HOST"
+docker run -v "$HOST":"$GUEST" "$CONTAINER_PUSH_REGISTRY"/"$DOCKER_NAME":"$DOCKER_IMAGE_TAG" "$GUEST"
+ls "$HOST"
