98835dd8cb989a824aa46cbb3711c5c5db27f0b7
Lott, Christopher (cl778h) <cl778h@att.com>
2019-09-27 06:01:54 -0400

    Add PyPI templates for RMR python binding
    Add triggers to limit library, binding builds appropriately.
    Clone NNG using a git commit SHA code, a version that fixes a bug.
    Extend pre-build scripts to get RMR version string from repo file.
    
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>
    Change-Id: Ie4b20163260179bff58ad958a7e8b87bbb9c02d2

--- jjb/shell/install-rpm-nng-rmr.sh
+++ jjb/shell/install-rpm-nng-rmr.sh
@@ -16,7 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Installs NNG from source and RMR from PackageCloud on CentOS
+# Installs NNG from source and RMR from PackageCloud on CentOS7
+# Reads RMR version number from repo file rmr-version.yaml
 
 echo "---> install-rpm-nng-rmr.sh"
 
@@ -27,17 +28,42 @@ sudo yum install -y \
     cmake3 \
     ninja-build
 
+echo "INFO: cd to tox-dir $TOX_DIR"
+cd "$WORKSPACE/$TOX_DIR"
+
+version_file=rmr-version.yaml
+if [[ -f $version_file ]]; then
+    # pipeline is less elegant than yq but that requires venv and pip install
+    ver=$(grep "^version:" "$version_file" | cut -d: -f2 | xargs )
+else
+    echo "File $version_file not found."
+    exit 1
+fi
+if [[ -z $ver ]]; then
+    echo "Failed to get RMR version string from file $version_file"
+    exit 1
+else
+    echo "RMR version string is ${ver}"
+fi
+
+# NNG repo is not frequently tagged so it's pinned to a commit hash.
+# This commit provides fix to the proxy-reconnect
+# bug that we identified:  https://github.com/nanomsg/nng/issues/970
 echo "Clone and build NNG"
-git clone --branch v1.1.1 https://github.com/nanomsg/nng.git
+git clone https://github.com/nanomsg/nng.git
 (cd nng \
+    && git checkout e618abf8f3db2a94269a79c8901a51148d48fcc2 \
     && mkdir build \
     && cd build \
     && cmake3 -DBUILD_SHARED_LIBS=1 -G Ninja .. \
     && ninja-build \
     && sudo ninja-build install)
 
-ver="1.4.0-1"
-echo "Download RMR library ${ver}"
-wget --content-disposition https://packagecloud.io/o-ran-sc/staging/packages/el/5/rmr-${ver}.x86_64.rpm/download.rpm
-echo "Install RMR library ${ver}"
-sudo rpm -vi rmr-${ver}.x86_64.rpm
+# RPM packager adds suffix "-1" to version
+rpm="rmr-${ver}-1.x86_64.rpm"
+echo "Download RMR library ${ver} as file ${rpm}"
+wget --content-disposition https://packagecloud.io/o-ran-sc/staging/packages/el/5/${rpm}/download.rpm
+echo "Install RMR library file ${rpm}"
+sudo rpm -iv ${rpm}
+
+echo "---> install-rpm-nng-rmr.sh ends"
