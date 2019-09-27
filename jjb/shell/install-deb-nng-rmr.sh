7ecb9aa8a25cfb557c7fc481fdd75027351ab365
Lott, Christopher (cl778h) <cl778h@att.com>
2019-09-27 06:01:54 -0400

    Add PyPI templates for RMR python binding
    Clone NNG using a git commit SHA code, a version that fixes a bug.
    Set pre-build scripts to use RMR ver 1.6.0, the most recent that passes tests.
    
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>
    Change-Id: Ie4b20163260179bff58ad958a7e8b87bbb9c02d2

--- jjb/shell/install-deb-nng-rmr.sh
+++ jjb/shell/install-deb-nng-rmr.sh
@@ -16,28 +16,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Installs NNG from source and RMR from PackageCloud on Ubuntu
+# Installs NNG from source and RMR from PackageCloud on Ubuntu18.04
 
 echo "---> install-deb-nng-rmr.sh"
 
 set -eu
 
 echo "Install packages"
-sudo apt-get update
-sudo apt-get install -y \
+sudo apt-get update && \
+    sudo apt-get install -y \
     cmake \
     ninja-build
 
+# NNG repo is not frequently tagged so it's pinned to a commit hash.
+# This commit provides fix to the proxy-reconnect
+# bug that we identified:  https://github.com/nanomsg/nng/issues/970
 echo "Clone and build NNG"
-git clone --branch v1.1.1 https://github.com/nanomsg/nng.git
+git clone https://github.com/nanomsg/nng.git
 (cd nng \
+    && git checkout e618abf8f3db2a94269a79c8901a51148d48fcc2 \
     && mkdir build \
     && cd build \
     && cmake -DBUILD_SHARED_LIBS=1 -G Ninja .. \
     && ninja \
     && sudo ninja install)
 
-ver="1.4.0"
+ver="1.6.0"
 echo "Download RMR library ${ver}"
 wget --content-disposition https://packagecloud.io/o-ran-sc/staging/packages/debian/stretch/rmr_${ver}_amd64.deb/download.deb
 echo "Install RMR library ${ver}"
