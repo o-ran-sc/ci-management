b0c6dd7c37d4efeb33e8df0b18b2b212bc6d9942
Bin Yang <Bin.Yang@windriver.com>
2019-11-14 06:59:16 +0000

    Add building job for pti rtp project
    Building artifacts only for now
    
    Issue-ID: INF-6
    Signed-off-by: Bin Yang <Bin.Yang@windriver.com>
    Change-Id: I793290f8dfd2fc439b593fd4ac84dd142e3596ee

---
+++ jjb/pti-rtp/build-pti-rtp-centos.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#   Copyright (C) 2019 Wind River Systems, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+# Installs prerequisites on CentOS then invokes the pti/rtp build script
+# to test creation of a Yocto image.  The "-n" flag skips bitbake.
+
+echo "--> build-pti-rtp-centos.sh"
+
+# Ensure we fail the job if any steps fail.
+set -eu -o pipefail
+
+pkgs="gawk make wget tar bzip2 gzip python unzip perl patch \
+     diffutils diffstat cpp gcc gcc-c++ glibc-devel texinfo chrpath socat \
+     perl-Data-Dumper perl-Text-ParseWords perl-Thread-Queue perl-Digest-SHA \
+     python3-pip xz which SDL-devel xterm"
+echo "INFO: installing epel-release and packages $pkgs"
+sudo yum install -y epel-release \
+  && sudo yum makecache \
+  && sudo yum install -y $pkgs
+
+dir=workspace
+echo "INFO: creating workspace $dir"
+mkdir $dir
+cmd="./scripts/build_oran.sh -w $dir"
+echo "INFO: invoking build script: $cmd"
+$cmd
+
+# now push artifacts to repo
+# tbd
+
+echo "--> build-pti-rtp-centos.sh ends"
