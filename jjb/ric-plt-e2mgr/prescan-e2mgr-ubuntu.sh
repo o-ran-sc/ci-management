bb8298020481ffac5a5d4c7d832ef231462b86bf
Lott, Christopher (cl778h) <cl778h@att.com>
2020-02-07 13:38:19 -0500

    Define Sonar job for E2Mgr which uses golang
    Use prescan builder shell script to install prereqs
    on an ubuntu build node
    
    Issue-ID: INT-81
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>
    Change-Id: Ia84f894a6a4b86afeabf3c66ac497f65444f159c

---
+++ jjb/ric-plt-e2mgr/prescan-e2mgr-ubuntu.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+##############################################################################
+#
+#   Copyright (c) 2020 AT&T Intellectual Property.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+##############################################################################
+
+# Installs NNG then runs a build script in the repository
+# Assumes ubuntu - uses apt-get
+	
+echo "--> prescan-e2mgr-ubuntu.sh"
+	
+set -ex
+	
+sudo apt-get update && sudo apt-get install -y cmake ninja-build
+	
+# NNG repo is not frequently tagged so it's pinned to a commit hash.
+# This commit repairs bug https://github.com/nanomsg/nng/issues/970
+git clone https://github.com/nanomsg/nng.git
+(cd nng \
+    && git checkout e618abf8f3db2a94269a79c8901a51148d48fcc2 \
+    && mkdir build \
+    && cd build \
+    && cmake -DBUILD_SHARED_LIBS=1 -G Ninja .. \
+    && ninja \
+    && sudo ninja install)
+	
+# build script must start in this subdir
+cd E2Manager
+bash ./build-e2mgr-ubuntu.sh
+	
+echo "--> prescan-e2mgr-ubuntu.sh ends"
