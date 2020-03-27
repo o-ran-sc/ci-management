e7234c874efeebbfd39a024ba225139b231e7b43
Lott, Christopher (cl778h) <cl778h@att.com>
2020-03-26 21:26:14 -0400

    Limit packagecloud-push with package prefix
    Add parameter package-prefix to limit the search for deb/rpm files
    inside the packagecloud-push script.  Previously it searched using
    wildcards *.deb/*.rpm and would try to upload all matches from the
    workspace.  Now it can be limited to files created by the job.
    
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>
    Change-Id: I3da170246fb5868e644756e80c20b07bf68f55f9

--- jjb/shell/packagecloud-push.sh
+++ jjb/shell/packagecloud-push.sh
@@ -8,34 +8,52 @@
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
 ##############################################################################
+
+# Prereqs and environment variables:
+# The build minion has the ruby gem "package_cloud"
+# The credentials file has been provisioned
+# DEBIAN_DISTRIBUTION_VERSIONS has distro list like "debian/stretch"
+# RPM_DISTRIBUTION_VERSIONS has distro list like "el/4 el/5"
+# PACKAGE_PREFIX has first part of package file names; e.g., "mylib"
+# PACKAGECLOUD_ACCOUNT is set and non-empty
+# PACKAGECLOUD_REPO is a value like "staging"
+
 echo "---> packagecloud-push.sh"
 set -eu -o pipefail
 
-if [ ! -f ~/.packagecloud ]; then
-    echo "INFO: .packagecloud file not found"
-    exit 0
+# Credentials for PackageCloud.io
+pc="~/.packagecloud"
+if [[ ! -f $pc ]]; then
+    echo "ERROR: credentials file $pc not found"
+    exit 1
 fi
+
+echo "Searching for files with prefix: $PACKAGE_PREFIX"
 
  # For DEB
 vers=("$DEBIAN_DISTRIBUTION_VERSIONS")
-echo "Debian distribution versions:" "${vers[@]}"
-debs=$(find . -type f -iname '*.deb')
+echo "Debian distribution versions: ${vers[@]}"
+debs=$(find . -type f -iname "${PACKAGE_PREFIX}*.deb")
+echo "Debian package files: $debs"
 # modern bash syntax is helpful
 for (( i = 0; i < ${#vers[@]}; i++ )); do
     for deb in $debs; do
-       echo "Pushing $deb $PACKAGECLOUD_ACCOUNT/$PACKAGECLOUD_REPO/${vers[i]}"
-       package_cloud push "$PACKAGECLOUD_ACCOUNT"/"$PACKAGECLOUD_REPO"/"${vers[i]}" "$deb"
+       arg="${PACKAGECLOUD_ACCOUNT}/${PACKAGECLOUD_REPO}/${vers[i]}"
+       echo "Pushing $deb as $arg"
+       package_cloud push "$arg" "$deb"
     done
 done
 
 # For RPM
 vers=("$RPM_DISTRIBUTION_VERSIONS")
-echo "RPM distribution versions:" "${vers[@]}"
-rpms=$(find . -type f -iregex '.*/.*\.\(s\)?rpm')
+echo "RPM distribution versions: ${vers[@]}"
+rpms=$(find . -type f -iname "${PACKAGE_PREFIX}*.rpm")
+echo "RPM package files: $rpms"
 # modern bash syntax is helpful
 for (( i = 0; i < ${#vers[@]}; i++ )); do
     for rpm in $rpms; do
-       echo "Pushing $rpm $PACKAGECLOUD_ACCOUNT/$PACKAGECLOUD_REPO/${vers[i]}"
-       package_cloud push "$PACKAGECLOUD_ACCOUNT"/"$PACKAGECLOUD_REPO"/"${vers[i]}" "$rpm"
+       arg="${PACKAGECLOUD_ACCOUNT}/${PACKAGECLOUD_REPO}/${vers[i]}"
+       echo "Pushing $rpm as $arg"
+       package_cloud push "$arg" "$rpm"
     done
 done
