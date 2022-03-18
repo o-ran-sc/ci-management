7c9c0dc30b22dfdcf9ebd6fbe3e18db62ef7c672
PatrikBuhr <patrik.buhr@est.tech>
2022-03-18 09:16:12 +0100

    NONRTRIC - Sonar support
    Change-Id: I41afa17af87ad739a5b22131073198ecda6c420e
    Signed-off-by: PatrikBuhr <patrik.buhr@est.tech>
    Issue-ID: NONRTRIC-737

---
+++ jjb/nonrtric/prescan-nonrtric-auth-token-fetch-ubuntu.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+##############################################################################
+#
+#   Copyright (C) 2021: Nordix Foundation
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+##############################################################################
+
+echo "--> $0"
+
+set -ex
+
+# Assumes ubuntu - uses apt-get
+sudo apt-get update
+
+# build script execution
+bash auth-token-fetch/run-tests-ubuntu.sh
+
+echo "--> $0 ends"
