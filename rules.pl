e15193f6e71b9db78657b6704babb2124cb14f9c
Andrew Grimberg <agrimberg@linuxfoundation.org>
2022-06-01 10:07:28 -0700

    Fix: Use the rules.pl from FDIO
    The rules.pl was copied from the LF Releng top level and that doesn't
    seem to work properly for a single repo
    
    Change-Id: I93ec4cc2556b96ee2b6fc0ac067e605865388c6b
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- rules.pl
+++ rules.pl
@@ -1,11 +1,16 @@
-submit_filter(In, Out) :-
-    In =.. [submit | Ls],
-    add_non_author_approval(Ls, R),
-    Out =.. [submit | R].
+
+% Require a non-author to vote it to completion
+submit_rule(S) :-
+  gerrit:default_submit(X),
+  X =.. [submit | Ls],
+  add_non_author_approval(Ls, R),
+  S =.. [submit | R].
 
 add_non_author_approval(S1, S2) :-
-    gerrit:commit_author(A),
-    gerrit:commit_label(label('Code-Review', 2), R),
-    R \= A, !,
-    S2 = [label('Non-Author-Code-Review', ok(R)) | S1].
+  gerrit:commit_author(A),
+  gerrit:commit_label(label('Code-Review', 2), R),
+  R \= A, !,
+  S2 = [label('Non-Author-Code-Review', ok(R)) | S1].
 add_non_author_approval(S1, [label('Non-Author-Code-Review', need(_)) | S1]).
+
+
