6f792b79fdd4220cb5c01d51b0b5998809347026
elinuxhenrik <henrik.b.andersson@est.tech>
2022-09-22 09:11:54 +0200

    Fix Sonar for nonrtric-controlpanel
    Issue-ID: NONRTRIC-651
    Signed-off-by: elinuxhenrik <henrik.b.andersson@est.tech>
    Change-Id: I18d8d7a9954f2520e80a51a5e1a210340d5ffbdf

---
+++ jjb/portal-nonrtric-controlpanel/frontend-prescan-controlpanel-ubuntu.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+##############################################################################
+#
+#   Copyright (C) 2022: Nordix Foundation
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+##############################################################################
+
+echo "--> frontend-prescan-controlpanel-ubuntu.sh"
+
+set -ex
+
+# Assumes ubuntu - uses apt-get
+sudo apt-get update
+sudo apt-get install curl
+# test execution
+cd webapp-frontend
+sudo rm -rf /var/lib/apt/lists/lock
+sudo curl -sL https://deb.nodesource.com/setup_13.x | sudo bash -
+sudo apt-get install -y nodejs
+sudo apt-get install -y chromium-browser
+export CHROME_BIN=/usr/bin/chromium-browser
+./ng test --browsers ChromeHeadless --code-coverage=true --watch=false
+
+echo "--> frontend-prescan-controlpanel-ubuntu.sh ends"
