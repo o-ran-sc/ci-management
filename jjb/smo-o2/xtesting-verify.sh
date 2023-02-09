9e851faef7c35b97607219569cd0a7357f879817
Koichi Edagawa <edagawa.kc@nec.com>
2023-02-09 10:25:50 +0900

    This patch is for validating creation of new job about Xtesting feature in smo-o2.
    This patch should not be reviewed because of only trial use.
    
    Signed-off-by: Koichi Edagawa <edagawa.kc@nec.com>
    Change-Id: I3eecb45593cca16a8cf49cca18cb1ffab0d2d7d7

---
+++ jjb/smo-o2/xtesting-verify.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git clone https://gerrit.o-ran-sc.org/r/ci-management
+cd ci-management/packer
+git rm common-packer
+COMMON_PACKER_VERSION=v0.14.1
+git submodule add https://github.com/lfit/releng-common-packer common-packer
+cd common-packer
+git checkout $COMMON_PACKER_VERSION
+
+sudo ./provision/install-python.sh
+sudo pip install ansible
+sudo ansible-playbook -i hosts ./provision/devstack.yaml
