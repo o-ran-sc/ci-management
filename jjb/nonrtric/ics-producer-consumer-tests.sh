8bfbf875c15fb67154b9c9999e0b0dbbecfb5fe1
JohnKeeney <john.keeney@est.tech>
2024-04-16 20:35:32 +0100

    Build and Verify job for ICS sample producer and consumer
    The start.sh script will return exit 1 if there are some errors and exit 0 if success.
    
    Issue-ID: NONRTRIC-965
    Change-Id: I6d7225f16ed07cd0fc35ef8aa2474e2207463b13
    Signed-off-by: JohnKeeney <john.keeney@est.tech>

---
+++ jjb/nonrtric/ics-producer-consumer-tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd sample-services/ics-producer-consumer || exit
+
+# Run the start.sh script and capture its exit status
+./start.sh
+start_status=$?
+
+# Check the exit status of the start.sh script
+if [ $start_status -eq 0 ]; then
+    echo "start.sh exited successfully (exit code 0)"
+else
+    echo "start.sh exited with an error (exit code $start_status)"
+fi
