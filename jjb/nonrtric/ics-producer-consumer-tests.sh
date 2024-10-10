ad837ac00c2eaccab1e0301fe0eb95f1abb83fae
lapentafd <francesco.lapenta@est.tech>
2024-10-10 14:06:05 +0100

    Updating sample rapp build job
    Issue-ID: NONRTRIC-1009
    Change-Id: I8b5925a3cca925e81c981fd065e13c3a5c665ec9
    Signed-off-by: lapentafd <francesco.lapenta@est.tech>

--- jjb/nonrtric/ics-producer-consumer-tests.sh
+++ jjb/nonrtric/ics-producer-consumer-tests.sh
@@ -2,8 +2,13 @@
 
 set -eux -o pipefail
 
-echo "Starting ics-producer-consumer-tests.sh which will execute sample-services/ics-producer-consumer/start.sh"
+echo "Starting 'ics-producer-consumer-tests.sh'"
+echo "Executing 'sample-services/ics-producer-consumer/start.sh'"
 cd sample-services/ics-producer-consumer
 bash start.sh
-
-echo "Finished ics-producer-consumer-tests.sh"
+echo "Finished Test 1"
+echo "Executing 'sample-services/ics-simple-producer-consumer/tests/start_ics_producer_consumer_test.sh'"
+cd ../../sample-services/ics-simple-producer-consumer/tests/
+bash start_ics_producer_consumer_test.sh
+echo "Finished Test 1"
+echo "Finished 'ics-producer-consumer-tests.sh'"
